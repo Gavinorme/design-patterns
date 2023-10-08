1f30568473d668da2f5b571b00286eb942bbf684
Gavinorme <Gorme@email.sc.edu>
2023-10-07 21:49:04 -0400

    iterator

--- iterator/Document.java
+++ iterator/Document.java
@@ -1,5 +1,8 @@
 package iterator;
-
+/**
+ * Document
+ * @author Gavin Orme
+ */
 import java.util.Iterator;
 import java.util.Stack;
 
@@ -8,11 +11,20 @@ public class Document {
     private Stack<String> undoStack = new Stack<String>();
     private Stack<String> redoStack = new Stack<String>();
 
+    /**
+     * allows useer to put in a document
+     * @param title creates a string for document 
+     */
     public Document(String title)
     {
         this.title = title;
     }
 
+    /**
+     * adds changes to the document and puts them on the undo command list
+     * @param change creates a string for what the user changes
+     * @return pushes the change the users makes the undo stack 
+     */
     public String makeChange(String change) //goes to undoStack
     {
         if(change == null)
@@ -23,6 +35,10 @@ public class Document {
             return undoStack.push(change);
     }
 
+    /**
+     * allows user to undo a chnage and puts it into the redo stack
+     * @return returns the string that is taken out of the top of the undo stack and adds it to the redo stack
+     */
     public String undoChange()
     {
         String poppedString = undoStack.pop();
@@ -30,11 +46,19 @@ public class Document {
         return poppedString;
     }
 
+    /**
+     * allows the user to undo if there are changes in the document
+     * @return returns when the undo stack is not empty which is all the time
+     */
     public boolean canUndo()
     {
         return !undoStack.empty();
     }
 
+    /**
+     * allows user to redo the change that they undid so a change in the redo stack can go back into the undo stack
+     * @return returns the string that is taken out of the top of the undo stack and adds it to the redo stack
+     */
     public String redoChange()
     {
         String poppedString = redoStack.pop();
@@ -42,31 +66,29 @@ public class Document {
         return poppedString;
     }
 
+    /**
+     * allows user to redo changes that they undid
+     * @return returns when the redo stack is not empty which is all the time
+     */
     public boolean canRedo()
     {
         return !redoStack.empty();
     }
 
+    /**
+     * creates a new undo stack
+     * @return returns the new undo stack
+     */
     public StackIterator getUndoIterator()
     {
         return new StackIterator(undoStack);
     }
+    /**
+     * creates a new redo stack
+     * @return returns the new redo stack
+     */
     public StackIterator getRedoIterator()
     {
         return new StackIterator(redoStack);
     }
-   
-
-}//maybe move
-
-
-//added changes go to the chnage list
-//undo goes to redo in chnage list
-//redo gives the undo back to the change list
-//Document
-//make chnage- undo
-//undochnage --> isEmpty
-//might not need position
-//getiterators prints out the list
-//import java.util.stack for document
-//
+}
--- iterator/StackIterator.java
+++ iterator/StackIterator.java
@@ -1,26 +1,40 @@
 package iterator;
-
+/**
+ * Iterator
+ * @author Gavin Orme
+ */
 import java.util.Stack;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-
 public class StackIterator implements Iterator {
     private List<String> stackItems = null;
     private int position = 0;
 
-    public StackIterator(Stack<String> stack) {
-        this.stackItems = new ArrayList<>(stack);
-        Collections.reverse(stackItems);
+    /**
+     * These strings are the changes to the document
+     * @param items an list of strings make up the stack
+     */
+    public StackIterator(Stack<String> items) {
+        this.stackItems = new ArrayList<>(items);
+        // Collections.reverse(stackItems);
     }
 
+    /** 
+     * adds to the position so it will go string by string
+     * @return returns the position of the stackItems array list and adds 1 for each item
+    */
     public String next()
     {
        return stackItems.get(position++);
     }
 
+    /**
+     * makes sure the index is in bounds
+     * @return returns true if the stackItems size is greater than the position
+     */
     public boolean hasNext()
     {
         if(position >= stackItems.size())
